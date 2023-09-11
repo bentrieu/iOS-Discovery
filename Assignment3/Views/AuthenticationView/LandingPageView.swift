39b684afedc59df8705767bbf0be76dd17c10875
Huu Phuoc <s3878270@rmit.edu.vn>
2023-09-11 22:41:14 +0700

    add authenticaiton view

---
+++ Assignment3/Views/AuthenticationView/LandingPageView.swift
@@ -0,0 +1,20 @@
+//
+//  LandingPageView.swift
+//  Assignment3
+//
+//  Created by Hữu Phước  on 11/09/2023.
+//
+
+import SwiftUI
+
+struct LandingPageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LandingPageView_Previews: PreviewProvider {
+    static var previews: some View {
+        LandingPageView()
+    }
+}
