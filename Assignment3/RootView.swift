7f3a5fb2e9419b6f786b656a4c8948fd1820ae3f
Ben Trieu <s3878466@rmit.edu.vn>
2023-09-16 15:31:09 +0700

    something

--- Assignment3/RootView.swift
+++ Assignment3/RootView.swift
@@ -15,7 +15,7 @@ struct RootView: View {
         ZStack {
             if !showSignInView {
                 NavigationStack {
-                    TempSettingsView(showSignInView: $showSignInView)
+                    TempProfileView(showSignInView: $showSignInView)
                 }
             }
         }
