69fa6f8f3e3b56cf3b4f267dab57aea4751ec64f
Ben Trieu <s3878466@rmit.edu.vn>
2023-09-24 11:14:48 +0700

    uncomment function

--- Assignment3/Firestore/PlaylistManager.swift
+++ Assignment3/Firestore/PlaylistManager.swift
@@ -48,14 +48,14 @@ final class PlaylistManager : ObservableObject{
         return try AuthenticationManager.instance.getAuthenticatedUser().uid
     }
     
-//        func getPlaylistsRef() async throws -> CollectionReference {
-//            return try await userCollection.document(getCurrentUser()).collection("playlists")
-//        }
-//
     func getPlaylistsRef() async throws -> CollectionReference {
-        return try await userCollection.document("mW8JeXNn48bI1annfiBM4x6pSdz2").collection("playlists")
+        return try await userCollection.document(getCurrentUser()).collection("playlists")
     }
-    
+
+//    func getPlaylistsRef() async throws -> CollectionReference {
+//        return try await userCollection.document("mW8JeXNn48bI1annfiBM4x6pSdz2").collection("playlists")
+//    }
+//    
     func addPlaylist() async throws {
         var playlistData: [String: Any] = [
             "playlist_id": "",
