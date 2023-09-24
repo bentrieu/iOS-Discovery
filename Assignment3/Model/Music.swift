f6d8c7db310531d2315280f77708470286c24847
Duy <s3836280@rmit.edu.vn>
2023-09-25 00:34:42 +0700

    comment model & utils

--- Assignment3/Model/Music.swift
+++ Assignment3/Model/Music.swift
@@ -1,16 +1,16 @@
 /*
-  RMIT University Vietnam
-  Course: COSC2659 iOS Development
-  Semester: 2023B
-  Assessment: Assignment 3
-  Author: Le Minh Quan, Dinh Huu Gia Phuoc, Vu Gia An, Trieu Hoang Khang, Nguyen Tran Khang Duy
-  ID: s3877969, s3878270, s3926888, s3878466, s3836280
-  Created  date: 10/9/2023
-  Last modified: 23/9/2023
-  Acknowledgement:
-https://rmit.instructure.com/courses/121597/pages/w9-whats-happening-this-week?module_item_id=5219569
-https://rmit.instructure.com/courses/121597/pages/w10-whats-happening-this-week?module_item_id=5219571
-*/
+ RMIT University Vietnam
+ Course: COSC2659 iOS Development
+ Semester: 2023B
+ Assessment: Assignment 3
+ Author: Le Minh Quan, Dinh Huu Gia Phuoc, Vu Gia An, Trieu Hoang Khang, Nguyen Tran Khang Duy
+ ID: s3877969, s3878270, s3926888, s3878466, s3836280
+ Created  date: 10/9/2023
+ Last modified: 23/9/2023
+ Acknowledgement:
+ https://rmit.instructure.com/courses/121597/pages/w9-whats-happening-this-week?module_item_id=5219569
+ https://rmit.instructure.com/courses/121597/pages/w10-whats-happening-this-week?module_item_id=5219571
+ */
 
 import Foundation
 import FirebaseFirestore
@@ -32,7 +32,7 @@ struct Music : Codable,Identifiable {
         case imageUrl = "image_url"
         case genre
     }
-
+    
 }
 enum Genre: String, CaseIterable{
     case KPOP = "K-pop"
@@ -71,6 +71,7 @@ class MusicViewModel : ObservableObject {
         print("get music in model")
     }
     
+    //fetch all music data from firebase and put in musics array
     func getAllMusicData() {
         db.collection("music").addSnapshotListener{(querySnapshot, error) in
             guard let documents = querySnapshot?.documents else {
@@ -91,6 +92,8 @@ class MusicViewModel : ObservableObject {
             }
         }
     }
+    
+    //find music object on firebase based on music id
     func findMusicById(_ musicId: String) async throws -> Music? {
         let query = db.collection("music").whereField("music_id", isEqualTo: musicId)
         
@@ -113,6 +116,8 @@ class MusicViewModel : ObservableObject {
         
         return music
     }
+    
+    //get list of musics in an album on firestore based on album id
     func getMusicListFromAlbum(_ albumId: String) async throws -> [Music] {
         let query = db.collection("album").whereField("album_id", isEqualTo: albumId)
         
@@ -148,7 +153,7 @@ class MusicViewModel : ObservableObject {
         return musics
     }
     
-    
+    //search music by name and artist from musics array based on the given input
     func searchMusicByNameAndArtist(input : String) -> [Music]{
         //filter with music name first
         var result = musics.filter { $0.musicName!.lowercased().contains(input.lowercased()) }
@@ -163,5 +168,5 @@ class MusicViewModel : ObservableObject {
         return result
     }
     
-   
+    
 }
